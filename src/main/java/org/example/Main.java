b1a595fe86202156f62630b3f4bf544533984bb0
Owner <tesfahundemasia47@gmail.com>
2023-01-15 18:02:06 +0200

    משימה 2 הוספתי good day

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,8 +8,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hello world!1");
-        System.out.println("Hello world! Hwllo tesfahon");
+        System.out.println("Hello world! Hello tesfahun");
+        System.out.println("good day");
+
 
     }
 }
