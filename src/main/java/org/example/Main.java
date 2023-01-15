5fc82a097e1077d1d6b059df3a3d70db3b842345
Owner <tesfahundemasia47@gmail.com>
2023-01-15 16:19:20 +0200

    commit code

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ *
+ *
+ * @author Tesfahun Demasia
+ * @id 337592646
+ * @date 15/01/23
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
