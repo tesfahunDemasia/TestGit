a5fe08475a764692ca7aafc6a207c9c73fb69fe8
trpyy <trpttnbrh@gmail.com>
2023-01-15 17:01:42 +0200

    הוספתי print

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,5 +9,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello world!1");
+        System.out.println("Hello world! Hwllo tesfahon");
+
     }
 }
